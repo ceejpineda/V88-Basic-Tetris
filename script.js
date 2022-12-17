8926057f68b36a4afa88f6c27d9ebd691ed6a31a
CJ <pinedacj1020@gmail.com>
2022-12-17 12:30:13 +0800

    Done basic functionality

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const tetris = (()=>{
 
     const gameGrid = document.querySelector('.gameGrid');
+    const nextPieceGrid = document.querySelector('.nextPieceGrid');
+
     let currentPos = 2;
-    let currentRotation = 0;    
+    let currentRotation = 0;
+    let randomizer = 0;
+    let score = 0;
     
     
     const spawnGrid = ()=>{
@@ -12,6 +16,21 @@ const tetris = (()=>{
             gridElement.dataset.pos = i;
             gameGrid.appendChild(gridElement);
         }
+        
+
+        for(let i=0; i<10; i++){
+            const gridElement = document.createElement('div');
+            gridElement.classList.add('gridElement');
+            gridElement.classList.add('floor');
+            gridElement.classList.add('bottom');
+            gameGrid.appendChild(gridElement);
+        }
+
+        // for(let i=0; i<16; i++){
+        //     const gridElement = document.createElement('div');
+        //     gridElement.classList.add('a');
+        //     nextPieceGrid.appendChild(gridElement);
+        // }
     }
 
     const spawnPiece = () =>{
@@ -22,13 +41,13 @@ const tetris = (()=>{
             [10,20,21,22],
         ]
         const lTetris = [
-            [1,12,22,2],
+            [0,11,21,1],
             [10,11,12,2],
-            [0,10,20,21],
-            [12,20,21,22],
+            [1,11,21,22],
+            [10,11,12,20],
         ]
         const tTetris = [
-            [1,2,3,12],
+            [1,10,11,12],
             [1,11,12,21],
             [10,11,12,21],
             [1,10,11,21],
@@ -58,58 +77,143 @@ const tetris = (()=>{
             [10,11,12,13],
         ]
 
-        const allPiece = [lTetris, jTetris, sTetris, zTetris, sTetris, iTetris, oTetris];
+        const allPiece = [lTetris, jTetris, tTetris, sTetris, zTetris, iTetris, oTetris];
+        randomizer = Math.floor(Math.random()*7);
         
-        return allPiece[Math.floor(Math.random()*7)];
+        return allPiece[randomizer];
     }
     
     let newPiece = spawnPiece();
     let current = newPiece[currentRotation];
+
+    const color = () =>{
+        if(randomizer == 0){
+            return '#0000ff';
+        }else if(randomizer == 1){
+            return '#ff7f00'
+        }else if(randomizer == 2){
+            return '#800080'
+        }else if(randomizer == 3){
+            return '#00FF00'
+        }else if(randomizer == 4){
+            return '#ff0000'
+        }else if(randomizer == 5){
+            return '#00ffff'
+        }else if(randomizer == 6){
+            return '#ffff00'
+        }
+    }
     
     const display = () =>{
         const gridElements = document.querySelectorAll('.gridElement');
         current.forEach(index =>{
             gridElements[currentPos + index].classList.add('piece');
-        })
+            gridElements[currentPos + index].style.backgroundColor = color();
+            });
     }
 
     const removePiece = () =>{
         const gridElements = document.querySelectorAll('.gridElement');
-        gridElements.forEach(element => {
-            if(element.classList.contains('piece')){
-                element.classList.remove('piece')
-            }
+        current.forEach(index => {
+            gridElements[currentPos + index].style.backgroundColor = 'rgba(127, 255, 212, 0.264)'
+            gridElements[currentPos + index].classList.remove('piece');
         });
     }
 
     const moveDown = () =>{
+        const gridElements = document.querySelectorAll('.gridElement');
+        if(!current.some(index => gridElements[currentPos+index+10].classList.contains('floor'))){
+            removePiece();
+            currentPos += 10;
+            display();
+        }
+        else{
+            isfloor();
+        }
+    }
+
+    const moveRight = () =>{
+        if(current.some(index =>(currentPos + index)%10 == 9)) return;
         removePiece();
-        currentPos += 10;
+        currentPos += 1;
+        display();
+    }
+
+    const moveLeft = () =>{
+        const gridElements = document.querySelectorAll('.gridElement');
+        removePiece();
+        if(!current.some(index =>(currentPos + index)%10 == 0)) {
+            currentPos -= 1;
+            if(current.some(index => gridElements[currentPos+index].classList.contains('floor'))){
+                currentPos += 1;
+            }
+        }
+        display();
+    }
+
+    const rotate = () =>{
+        currentRotation++;
+        if(currentRotation == 4){
+            currentRotation = 0;
+        }
+        removePiece();
+        current = newPiece[currentRotation];
         display();
     }
 
     const controls = () =>{
+        let rightEdge = false;
         document.addEventListener('keydown', (e)=>{
             if(e.key == "ArrowRight"){
-                removePiece();
-                currentPos += 1;
-                display();
+                moveRight();
             }if(e.key == "ArrowLeft"){
-                removePiece();
-                currentPos -= 1;
-                display();
+                moveLeft();
             }if(e.key == "ArrowDown"){
                 moveDown();
             }if(e.key == "ArrowUp"){
-                currentRotation++;
-                if(currentRotation == 4){
-                    currentRotation = 0;
-                }
-                current = newPiece[currentRotation];
-                removePiece();
-                display();
+                rotate();
+            }if(e.key == " "){
+                moveDown();
             }
         });
+    }
+
+    const isfloor = () =>{
+        const gridElements = document.querySelectorAll('.gridElement');
+        if(current.some(index => gridElements[currentPos + index + 10].classList.contains('floor'))){
+            current.forEach(index => gridElements[currentPos + index].classList.add('floor'))
+            console.log('hello');
+            newPiece = spawnPiece();
+            current = newPiece[currentRotation];
+            currentPos = 4;
+            display();
+            scoreMechanic();
+        }
+    }
+
+    const scoreMechanic = () =>{
+        let gridElements = Array.from(document.querySelectorAll('.gridElement'));
+        console.log(gridElements)
+
+        for(let i = 0; i<180; i+=10){
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+            console.log(row.every(j => gridElements[j].classList.contains('floor')))
+            if(row.every(j => gridElements[j].classList.contains('floor'))){
+                score += 10;
+                console.log(score);
+                row.forEach(index =>{
+                    gridElements[index].classList.remove('floor');
+                    gridElements[index].classList.remove('piece');
+                    gridElements[index].style.backgroundColor = "rgba(127, 255, 212, 0.264)";
+
+                })
+                const refreshedBlocks = gridElements.splice(i, 10);
+                gridElements = refreshedBlocks.concat(gridElements);
+                gridElements.forEach(cell=>{
+                    gameGrid.appendChild(cell);
+                })
+            }
+        }
     }
     
     return {spawnGrid, display, removePiece, controls, moveDown}
@@ -119,4 +223,4 @@ const tetris = (()=>{
 tetris.spawnGrid();
 tetris.display();
 tetris.controls();
-setInterval(tetris.moveDown, 1000)
+tetris.moveDown();
